4688cf959c564f772e4cbdfb1946e3edb52e58e1
Sarah Krstyen <sarah.krstyen@gmail.com>
2014-04-20 15:16:24 -0600

    another commit

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
 	name='densityestimation',
-	version='0.1.0',
+	version='0.1.1',
 	authors=['B. Aldous, S. Krstyen'],
 	author_emails=['benaldous@yahoo.com, sarah.krstyen@gmail.com'],
 	package='densityestimation',
-	#url='',
+	url='https://pypi.python.org/pypi?%3Aaction=pkg_edit&name=densityestimation',
 	license='MIT',
 	description='Density estimation functions.',
 	long_description=open('README.txt').read(),
